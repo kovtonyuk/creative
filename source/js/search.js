445c18a8b6222997eb9edb068721ced6680d05cb
kovtonyuk <s.kovtonyuk@gmail.com>
2018-10-08 10:53:01 +0300

    add search in menu

---
+++ source/js/search.js
@@ -0,0 +1,13 @@
+$(function() {
+    $(".nav-menu > .menu .search .s-b").on("click", function(e) {
+        e.preventDefault();
+        $("html").addClass("search-exp");
+        $(".s-i").focus();
+    });
+    $(".s-i").blur(function() {
+        // Do not hide input if contains text
+        if ($(".s-i").val() === "") {
+            $("html").removeClass("search-exp");
+        }
+    });
+})
