a189f8f2d379bcd8034a3982b7d2ccfa1ffef8e6
kovtonyuk <s.kovtonyuk@gmail.com>
2018-10-10 09:10:45 +0300

    create sort menu

--- source/js/sorting_portfolio.js
+++ source/js/sorting_portfolio.js
@@ -1,38 +1,6 @@
-// var fActive = '';
-//
-// function filterPortfolioItems(item){
-//     if(fActive != item){
-//         $('div').filter('.'+item).slideDown();
-//         $('div').filter(':not(.'+item+')').slideUp();
-//         fActive = item;
-//     }
-// }
-//
-// $('.f-logo').click(function(){ filterPortfolioItems('logo'); });
-// $('.f-mobile_app').click(function(){ filterPortfolioItems('mobile_app'); });
-// $('.f-wordpress').click(function(){ filterPortfolioItems('wordpress'); });
-// $('.f-web_design').click(function(){ filterPortfolioItems('web_design'); });
-// $('.f-ui_ux').click(function(){ filterPortfolioItems('ui_ux'); });
-// $('.f-branding').click(function(){ filterPortfolioItems('branding'); });
-//
-// $('.f-all').click(function(){
-//     $('div').slideDown();
-//     fActive = 'all';
-// });
-
-
 $('[data-id]').click(function(){
     var items = $('.portfolio__wrapper > div');
     var id = $(this).attr('data-id');
     items.hide();
     $('.'+id).show();
 });
-
-
-
-// $('[data-id]').click(function(){
-//     var items = $('.portfolio__wrapper div');
-//     var id = $(this).attr('data-id');
-//     items.hide();
-//     $('.'+id).show();
-// });
