effbf3b73d8f570b493493b8089e253cbe29d422
kovtonyuk <vika_serhii2606>
2018-10-13 23:39:55 +0300

    create blog page

--- source/js/read_more.js
+++ source/js/read_more.js
@@ -1,3 +1,4 @@
+/*----- Load more on Home and Portfolio pages -----*/
 $(document).ready(function() {
     $("#toggle").click(function() {
         var elem = $("#toggle").text();
@@ -8,6 +9,23 @@ $(document).ready(function() {
         } else {
             //Stuff to do when btn is in the read less state
             $("#toggle").text("load more");
+            $(".hidden").slideUp();
+        }
+    });
+});
+
+/*----- Load more on Blog page -----*/
+$(document).ready(function() {
+    $("#toggle_blog").click(function() {
+        var elem = $("#toggle_blog").html();
+        if (elem == '<i class="fas fa-angle-down"></i>') {
+            //Stuff to do when btn is in the read more state
+            $("#toggle_blog").html('<i class="fas fa-angle-up"></i>');
+            $(".hidden").slideDown();
+            $(".hidden").addClass("flex");
+        } else {
+            //Stuff to do when btn is in the read less state
+            $("#toggle_blog").html('<i class="fas fa-angle-down"></i>');
             $(".hidden").slideUp();
         }
     });
