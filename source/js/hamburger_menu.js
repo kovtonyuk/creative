016fda1fa787d5c37a23430c68c529fed52f621b
kovtonyuk <s.kovtonyuk@gmail.com>
2018-10-17 16:24:49 +0300

    mobile style

--- source/js/hamburger_menu.js
+++ source/js/hamburger_menu.js
@@ -1,20 +1,24 @@
-$(function() {
-    menu = $('.menu ul');
 
-    $('#openup').on('click', function(e) {
-        e.preventDefault(); menu.slideToggle();
+$(document).ready(function() {
+    //responsive menu toggle
+    $("#menutoggle").click(function() {
+        $('.xs-menu').toggleClass('displaynone');
+
+    });
+    //add active class on menu
+    $('ul li').click(function(e) {
+        e.preventDefault();
+        $('li').removeClass('active');
+        $(this).addClass('active');
+    });
+    //drop down menu
+    $(".drop-down").hover(function() {
+        $('.mega-menu').addClass('display-on');
+    });
+    $(".drop-down").mouseleave(function() {
+        $('.mega-menu').removeClass('display-on');
     });
 
-    $(window).resize(function(){
-        var w = $(this).width(); if(w > 480 && menu.is(':hidden')) {
-            menu.removeAttr('style');
-        }
-    });
-
-    $('nav li').on('click', function(e) {
-        var w = $(window).width(); if(w < 480 ) {
-            menu.slideToggle();
-        }
-    });
-    $('.open-menu').height($(window).height());
 });
+
+	 
